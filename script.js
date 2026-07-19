c23e428e5c105521b71d03c7d5208d72b7720ec5
deepstudios <ishansaji14@gmail.com>
2026-07-19 17:33:10 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Deep Games Loaded!");
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        alert("Game page coming soon!");
+    });
+});
